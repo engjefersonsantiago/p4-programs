8ce724a9cd83d2a2424a6674b63f5b465f60311b
Jeferson Santiago da Silva <eng.jefersonsantiago@gmail.com>
2017-05-04 12:12:17 -0400

    Corrected calc functions

--- examples/rohc_compression/parse_and_calculate.py
+++ examples/rohc_compression/parse_and_calculate.py
@@ -3,36 +3,56 @@ import os.path
 import re
 from struct import *
 import math
+import numpy
+import np
+
 def main():
     
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         param = len(sys.argv) - 1
-        print "Invalid parameter number. Expected at least 2 got %d" % param
-        print "Usage python parser_and_calculate.py <log_file> <regex1>"
+        print "Invalid parameter number. Expected at least 3 got %d" % param
+        print "Usage python parser_and_calculate.py <log_file> <regex1> <samples_num>"
         return
     
     file_log = sys.argv[1] 
     file_ = open(file_log, "r")
     patern = sys.argv[2]
-
+    samples = sys.argv[3]
+    print(samples)
+    first_match = True
+    cnt = 0
     time_array = []
     for line in file_:
-       if re.match(patern, line):
-           time = str(re.sub(patern, '', line)).strip()
-           time = int(re.match("[0-9]+", time).group(0).strip())
-           time_array.append(time)
-    
+        if cnt < int(samples):
+            #print(cnt)
+            if re.match(patern, line):
+                if not first_match:
+                    time = str(re.sub(patern, '', line)).strip()
+                    time = int(re.match("[0-9]+", time).group(0).strip())
+                    time_array.append(time)
+                    cnt = cnt + 1
+                else:
+                    first_match = not first_match
+        else:
+            break
+
+    #fft_ = []
+    #fft_ = np.fft.fft(time_array)
+    #print(fft_) 
     avg = sum(time_array)/len(time_array)
-    print "Average time for #%d packets: %d usec" % (len(time_array), avg)
+    print "Average time for #%d packets: %f usec" % (len(time_array), avg)
     sigma_sq = []
     for i in time_array:
-        sigma_sq.append(i**2-avg**2)
-    
-    sigma_sq_mean = sum(sigma_sq)/len(sigma_sq)
-    
-    print "Mean variance for #%d packets: %d usec" % (len(sigma_sq), sigma_sq_mean)
-    print "Mean Deviation for #%d packets: %d usec" % (len(sigma_sq), math.sqrt(sigma_sq_mean))
+        sigma_sq.append((i-avg)**2)
 
+    sigma_sq_mean = sum(sigma_sq)/len(sigma_sq)/len(sigma_sq)
+    
+    print "Mean variance for #%d packets: %f usec" % (len(sigma_sq), sigma_sq_mean)
+    print "Mean Deviation for #%d packets: %f usec" % (len(sigma_sq), math.sqrt(sigma_sq_mean))
+
+    #for i in time_array:
+    #    if i < avg - 5*math.sqrt(sigma_sq_mean) or i > avg + 5*math.sqrt(sigma_sq_mean):
+    #        print(i)
 
 if __name__ == '__main__':
     main()
--- examples/rohc_compression/parse_and_calculate_multiple.py
+++ examples/rohc_compression/parse_and_calculate_multiple.py
@@ -6,10 +6,10 @@ import math
 
 def main():
     
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         param = len(sys.argv) - 1
-        print "Invalid parameter number. Expected at least 2 got %d" % param
-        print "Usage python parser_and_calculate.py <log_file> <regex1 regex2>"
+        print "Invalid parameter number. Expected at least 3 got %d" % param
+        print "Usage python parser_and_calculate.py <log_file> <regex1> <samples_num>"
         return
     
     file_log = sys.argv[1] 
@@ -18,31 +18,44 @@ def main():
     for i in range(2, len(sys.argv)):
         patern_array.append(sys.argv[i])
 
+    samples = sys.argv[4]
     time_array = []
     tgl = False
+    first_match = True
     time_old = 0
+    cnt = 0
     for line in file_:
-        for patern in patern_array:
-            if re.match(patern, line):
-                time = str(re.sub(patern, '', line)).strip()
-                time = int(re.match("[0-9]+", time).group(0).strip())
-                if not tgl:
-                    time_old = time
-                else:
-                    time_array.append(time + time_old)
-                tgl = not tgl
-    
+        if cnt < int(samples):
+            for patern in patern_array:
+                if re.match(patern, line):
+                    time = str(re.sub(patern, '', line)).strip()
+                    time = int(re.match("[0-9]+", time).group(0).strip())
+                    if not tgl:
+                        time_old = time
+                    else:
+                        if not first_match:
+                            time_array.append(time + time_old)
+                            cnt += 1
+                        else:
+                            first_match = not first_match
+                    tgl = not tgl
+        else:
+            break
+ 
     avg = sum(time_array)/len(time_array)
-    print "Average time for #%d packets: %d usec" % (len(time_array), avg)
+    print "Average time for #%d packets: %f usec" % (len(time_array), avg)
 
     sigma_sq = []
     for i in time_array:
-        sigma_sq.append(i**2-avg**2)
+        sigma_sq.append((i-avg)**2)
     
-    sigma_sq_mean = sum(sigma_sq)/len(sigma_sq)
+    sigma_sq_mean = sum(sigma_sq)/len(sigma_sq)/len(sigma_sq)
     
-    print "Mean variance for #%d packets: %d usec" % (len(sigma_sq), sigma_sq_mean)
-    print "Mean Deviation for #%d packets: %d usec" % (len(sigma_sq), math.sqrt(sigma_sq_mean))
+    print "Mean variance for #%d packets: %f usec" % (len(sigma_sq), sigma_sq_mean)
+    print "Mean Deviation for #%d packets: %f usec" % (len(sigma_sq), math.sqrt(sigma_sq_mean))
+    #for i in time_array:
+    #    if i < avg - 5*math.sqrt(sigma_sq_mean) or i > avg + 5*math.sqrt(sigma_sq_mean):
+    #        print(i)
 
 if __name__ == '__main__':
     main()
